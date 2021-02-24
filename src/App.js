2f0e700a80032329d0382665cf03ffc88e49aaf7
Rimma <primma1984@mail.ru>
2021-02-24 10:51:13 +0700

    урок React-элемент

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-import { count, lengt } from './number.js';
+const element = (<div>
+  Hello World!
+</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-         {count * lengt}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
