94a21c8c581b6941cc77f3ffdbbe74a731812758
Rimma <primma1984@mail.ru>
2021-02-25 20:36:21 +0700

    урок Props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
+
+const toDoItem = 'Написать новое приложение';
 
 const App = () => (
 <div>
   <h1>Важные дела:</h1>
-  <ItemList />
+  <InputItem />
+  <ItemList toDoItem={toDoItem} />
+  <Footer count={3} />
 </div>);
 
 export default App;
