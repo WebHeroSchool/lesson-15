4cd934486413b84cb04c8a57ce3de7f8e958899b
Rimma <primma1984@mail.ru>
2021-02-26 16:10:50 +0700

    урок "Вывод списка элементов и ключи"

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = ({ toDoItem }) => (<ul>
- <li><Item toDoItem={toDoItem} /></li>
- <li><Item toDoItem={'прописать props-ы'} /></li>
- <li><Item toDoItem={'сделать все дела'} /></li>
+const ItemList = ({ items }) => (<ul>
+  {items.map(item => <li key={item.value}>
+      <Item value={item.value} />
+    </li>)}
 </ul>);
 
 export default ItemList;
