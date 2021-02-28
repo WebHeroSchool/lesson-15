66e50f3d206088b209fcb528c81709cf6d04a585
Rimma <primma1984@mail.ru>
2021-02-28 14:10:40 +0700

    урок "Готовые библиотеки компонентов. Material UI"

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (
-  <div></div>
-);
+const InputItem = () => (<div>
+  <TextField
+    id="standart-dense"
+    label="Добавить задание"
+    margin="dense"
+  />
+</div>);
 
 export default InputItem;
