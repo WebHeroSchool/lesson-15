627f4eceeaf8bb134bf98bdc95e55e65070e4464
Rimma <primma1984@mail.ru>
2021-03-07 21:24:42 +0700

    урок "Методы жизненного цикла"

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -6,50 +6,66 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const ItemList = ({ items, onClickDone, onClickDelete }) => (
-  <ul className={styles.itemList}>
-  {items.map(item =>
-    <li key={item.id} className={styles.item}>
-    <Checkbox
-      checked={item.isDone}
-      color="primary"
-      value="checkedG"
-      inputProps={{
-        'aria-label': 'secondary checkbox',
-      }}
-      onClick={() => onClickDone(item.id)}
-     />
-     <div className={styles.itemText} onClick={() => onClickDone(item.id)}>
-       <Item
-         value={item.value}
-         isDone={item.isDone}
-         id={items.id}
-         onClickDone={onClickDone}
+class ItemList extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  compomentDidUpdate() {
+    console.log('compomentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  
+  render() {
+    const { items, onClickDone, onClickDelete } = this.props;
+
+    return (<ul className={styles.itemList}>
+      {items.map(item =>
+        <li key={item.id} className={styles.item}>
+        <Checkbox
+          checked={item.isDone}
+          color="primary"
+          value="checkedG"
+          inputProps={{
+          'aria-label': 'secondary checkbox',
+        }}
+        onClick={() => onClickDone(item.id)}
         />
-     </div>
-     <div>
-       <IconButton
-         aria-label='delete'
-         onClick={() => onClickDelete(item.id)}
-        >
-         <DeleteIcon fontSize='small' />
-       </IconButton>
-     </div>
-    </li>)}
-  </ul>
-);
+        <div className={styles.itemText} onClick={() => onClickDone(item.id)}>
+          <Item
+            value={item.value}
+            isDone={item.isDone}
+            id={items.id}
+            onClickDone={onClickDone}
+          />
+        </div>
+        <div>
+          <IconButton
+            aria-label='delete'
+            onClick={() => onClickDelete(item.id)}
+          >
+          <DeleteIcon fontSize='small' />
+          </IconButton>
+        </div>
+      </li>)}
+    </ul>
+  );
 
-ItemList.defaultProps = {
-  items: [{
-    value: "Не существующая задача",
-    isDone: false,
-  }]
-};
+  ItemList.defaultProps = {
+    items: [{
+      value: "Не существующая задача",
+      isDone: false,
+    }]
+  };
 
-ItemList.propTypes = {
-	items: PropTypes.array.isRequired,
-	onClickDone: PropTypes.func,
-	onClickDelete: PropTypes.func
+  ItemList.propTypes = {
+  	items: PropTypes.array.isRequired,
+  	onClickDone: PropTypes.func,
+  	onClickDelete: PropTypes.func};
+  }
 };
 
 export default ItemList;
