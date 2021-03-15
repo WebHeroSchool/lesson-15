828207b21a67cd9d37f1af780ca44720ac894ac5
Rimma <primma1984@mail.ru>
2021-03-15 22:31:43 +0700

    урок "Запрос данных с сервера"

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Octokit from '@octokit/rest';
 
-const About = () => (
-  <div>
-    <h1>About</h1>
-  </div>
-);
+const octokit = new Octokit();
+
+class About extends React.Component {
+  state = {
+    isLoading: true,
+    repolist: []
+  }
+
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'RimmaMaybo'
+    }).then(({ data }) => {
+      this.setState({
+        repolist: data,
+        isLoading: false
+      });
+    });
+  }
+
+  render() {
+    const { isLoading, repolist } = this.state;
+
+    return (
+      <CardContent>
+        <h1>{ isLoading ? <CircularProgress /> : 'Мои репозитории' }</h1>
+        {!isLoading && <ol>
+            {repolist.map(repo => (<li key={repo.id}>
+                {repo.name}
+              </li>))}
+          </ol>}
+      </CardContent>
+    );
+  }
+}
 
 export default About;
